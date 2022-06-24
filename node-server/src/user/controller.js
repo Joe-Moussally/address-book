7fb539f963af090db2cdb948c4e4b865a79601f6
MJoe111 <joemoussally111@gmail.com>
2022-06-24 03:19:14 +0300

    Adding JTW on register

--- node-server/src/user/controller.js
+++ node-server/src/user/controller.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const User = require('../../models/User');
+
+const TOKEN_SECRET = process.env.TOKEN_SECRET || ""
 
 const { createUser, getUserByEmail } = require('./service')
 
@@ -7,6 +10,13 @@ async function register(req, res) {
 
     try {
         console.log(req.body)
+        const email = req.body.email
+
+        //check if email  already exists
+        const oldUser = await User.findOne({email})
+        if(oldUser) {
+            return res.status(409).send('email already exists')
+        }
 
         const salt = await bcrypt.genSalt(5)
         const hashPassword = await bcrypt.hash(req.body.password,salt)
@@ -14,7 +24,19 @@ async function register(req, res) {
         const addedUser = await createUser(req.body)
         console.log('result => ',addedUser)
 
-        return res.send({user:addedUser._id})
+        //create token
+        const token = jwt.sign(
+            {user_id: addedUser._id,email},
+            TOKEN_SECRET,
+            {
+                expiresIn: '1h'
+            }
+        )
+
+        return res.send({
+            user:addedUser._id,
+            token:token 
+        })
 
     } catch(err) {
         console.log(err)
