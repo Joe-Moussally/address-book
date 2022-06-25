2d3b5b9ee07d6ad42037e8b7a6593a94170fd080
MJoe111 <joemoussally111@gmail.com>
2022-06-26 02:54:12 +0300

    Adding logout

--- react-frontend/src/pages/Contacts.js
+++ react-frontend/src/pages/Contacts.js
@@ -1,17 +1,26 @@
 import { useEffect, useState } from "react"
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import Contact from "../components/Contact"
 import LeafMap from "../components/LeafMap"
 
 const Contacts = () => {
 
+    const nav = useNavigate()
+        
     const [contacts,setContacts] = useState([])
 
     //fetch all contacts
     useEffect(()=>{
+
+        const token = localStorage.getItem('token')
+
+        if(!token){
+            nav('/')
+        }
         axios({
             headers:{
-                authorization: localStorage.getItem('token')
+                authorization: token
             },
             method:'GET',
             url: 'http://localhost:27017/api/contact'
@@ -21,13 +30,18 @@ const Contacts = () => {
         })
     },[])
 
+    const handleLogout = () => {
+        nav('/login')
+        localStorage.setItem('token',null)
+    }
+
     return ( 
         <>
             <div id="contacts-container" className="container">
                 <h1>My Contacts</h1>
 
                 <button id="add-contact-btn">Add Contact</button>
-                <button id="logout-btn">Log Out</button>                
+                <button id="logout-btn" onClick={handleLogout}>Log Out</button>                
 
                 <div>
                     {
